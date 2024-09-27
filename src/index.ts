ce47212c74ad63a56e56c2bdd7f187c10816056e
Alan Wang <wy.dong96@gmail.com>
2024-09-27 15:53:14 +0800

    add res time logger

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,24 @@ import process from 'node:process'
 import { sentry } from '@hono/sentry'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-// import { logger } from 'hono/logger'
 import httpStatus from 'http-status'
 import { type Environment } from '../bindings'
 import { errorHandler } from './middlewares/error'
 import { defaultRoutes } from './routes'
 import { ApiError } from './utils/ApiError'
+import { logger } from './utils/logger'
+import { resTimeLogger } from '@/middlewares/resTimeLogger'
 // export { RateLimiter } from './durable-objects/rateLimiter.do'
 
 const app = new Hono<Environment>()
-
-// app.use(logger())
 
 // Middleware to bind env vars to process.env
 app.use('*', (c, next) => {
   Object.assign(process.env, c.env) // Directly assign env vars
   return next()
 })
+
+app.use(resTimeLogger())
 
 app.use('*', sentry())
 app.use('*', cors())
@@ -33,4 +34,5 @@ defaultRoutes.forEach((route) => {
   app.route(`${route.path}`, route.route)
 })
 
+logger().info('Server is running')
 export default app
---
+++ src/middlewares/resTimeLogger.ts
@@ -0,0 +1,81 @@
+/**
+ * @module
+ * Logger Middleware for Hono.
+ */
+
+import type { MiddlewareHandler } from 'hono/types'
+import { getColorEnabled } from 'hono/utils/color'
+import { getPath } from 'hono/utils/url'
+import { logger as pinoLogger } from '@/utils/logger'
+enum LogPrefix {
+  Outgoing = '-->',
+  Incoming = '<--',
+  Error = 'xxx'
+}
+
+const time = (start: number) => {
+  const delta = Date.now() - start
+  return delta < 1000 ? delta + 'ms' : Math.round(delta / 1000) + '.' + (delta % 1000) + 's'
+}
+
+const colorStatus = (status: number) => {
+  const colorEnabled = getColorEnabled()
+  const out: { [key: string]: string } = {
+    7: colorEnabled ? `\x1b[35m${status}\x1b[0m` : `${status}`,
+    5: colorEnabled ? `\x1b[31m${status}\x1b[0m` : `${status}`,
+    4: colorEnabled ? `\x1b[33m${status}\x1b[0m` : `${status}`,
+    3: colorEnabled ? `\x1b[36m${status}\x1b[0m` : `${status}`,
+    2: colorEnabled ? `\x1b[32m${status}\x1b[0m` : `${status}`,
+    1: colorEnabled ? `\x1b[32m${status}\x1b[0m` : `${status}`,
+    0: colorEnabled ? `\x1b[33m${status}\x1b[0m` : `${status}`
+  }
+
+  const calculateStatus = (status / 100) | 0
+
+  return out[calculateStatus]
+}
+
+function logMessage(
+  prefix: string,
+  method: string,
+  path: string,
+  status: number = 0,
+  elapsed?: string
+) {
+  const out =
+    prefix === LogPrefix.Incoming
+      ? `${method} ${path}`
+      : `${method} ${path} ${colorStatus(status)} ${elapsed}`
+  return out
+}
+
+/**
+ * Res Time Logger Middleware for Hono.
+ *
+ * @see {@link https://hono.dev/docs/middleware/builtin/logger}
+ *
+ * @param {PrintFunc} [fn=console.log] - Optional function for customized logging behavior.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * const app = new Hono()
+ *
+ * app.use(logger())
+ * app.get('/', (c) => c.text('Hello Hono!'))
+ * ```
+ */
+export const resTimeLogger = (): MiddlewareHandler => {
+  return async function logger(c, next) {
+    const { method } = c.req
+
+    const path = getPath(c.req.raw)
+
+    const start = Date.now()
+
+    await next()
+
+    const msg = logMessage(LogPrefix.Outgoing, method, path, c.res.status, time(start))
+    pinoLogger().info(msg)
+  }
+}
